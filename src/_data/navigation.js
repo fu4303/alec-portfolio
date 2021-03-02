026ccc42bdcdc3e906ce9bc864c542cd78d90a40
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-03-02 13:26:53 -0500

    Update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -3,9 +3,10 @@ module.exports = {
     { name: 'Contact', link: '/contact/' },
     { name: 'Sites', link: '/sites/' },
     { name: 'Blog', link: '/blog/' },
+    { name: 'Bookmarks', link: '/bookmarks/' },
   ],
   external: [
-    { icon: 'github', link: 'https://github.com/mfu4303' },
+    { icon: 'github', link: 'https://github.com/fu4303' },
     { icon: 'linkedin', link: 'https://www.linkedin.com/in/doc22940/' },
     { icon: 'dev', link: 'https://dev.to/doc22940' },
   ],
