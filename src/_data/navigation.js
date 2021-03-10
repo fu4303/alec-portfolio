ed3c822db3089a5ef7dff708763dfdca7cd338fe
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-03-10 11:46:31 -0500

    Update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,9 +1,10 @@
 module.exports = {
   internal: [
-    { name: 'Contact', link: '/contact/' },
     { name: 'Sites', link: '/sites/' },
     { name: 'Blog', link: '/blog/' },
     { name: 'Bookmarks', link: '/bookmarks/' },
+    { name: 'Quotes', link: '/quotes/' },
+    { name: 'Contact', link: '/contact/' },
   ],
   external: [
     { icon: 'github', link: 'https://github.com/fu4303' },
